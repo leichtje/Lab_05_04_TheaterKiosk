d869d6caf21d5af6a3f7b140ff5810fa4d3edb7e
leichtje <113708612+leichtje@users.noreply.github.com>
2022-09-24 12:58:57 -0400

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,15 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+
+        int age;
+        int WRIST_BAND_AGE=21;
+
+        System.out.println("Please enter your age as a whole number");
+        age=18;
+        System.out.println("You are " + age + " years old");
+
+        if (age>=WRIST_BAND_AGE){
+            System.out.println("You get a Wrist band");
+        }
+    }
+}
